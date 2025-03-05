3b70adf8c4745e43aa9955c2e9129603c4207e3f
Correia2911 <gelboy71@gmail.com>
2025-03-05 18:39:44 +0000

    The Functions Assignment in JS

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,14 @@
+// Create the first string variable
+let Sent1 = "This is the beginning of the string";
+
+// Create the second string variable
+let Sent2 = " and This is the end of the string";
+
+// Concatenate the two variables together
+let completeString = Sent1 + Sent2; // Combine Sent1 and Sent2 into one string
+
+// Use the window.alert() method to display the complete string in an alert box
+window.alert(completeString);
+
+// Use the document.write() method to write the complete string on the webpage
+document.write(completeString); // This will display the concatenated string on the HTML page
